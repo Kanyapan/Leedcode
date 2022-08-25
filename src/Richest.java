d279fc27ef7667b6ccd97109a0d4a74c91586c65
Kanyapan VACHARASILP <ying.titans@gmail.com>
2022-08-25 21:54:14 +0700

    LeedcodeRunningSumOf1dArray

--- src/Richest.java
+++ src/Richest.java
@@ -1,2 +1,15 @@
-public class Richest {
+/*public class Richest {
+    int ans=0;
+
+    for(int i=0;i<accounts.length;i++){
+        int total=0;
+        for(int j=0;j<accounts[i].length;j++){
+
+            total=total+accounts[i][j];
+        }
+        if(total>=ans){
+            ans=total;
+        }
+    }
+    return ans;
 }
